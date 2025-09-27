325a4d0845d08ae2ebb02ad0fa74bf06bb2ff76e
Asmee-tech <asmee.khimani@gmail.com>
2025-09-27 09:58:00 +0200

    Add files via upload

---
+++ bouncy_ball.py
@@ -0,0 +1,44 @@
+import pgzrun
+import random
+TITLE="Bouncy balls!"
+WIDTH=800
+HEIGHT=800
+grav=2000.0
+#ball class
+class bouncy():
+    def __init__(self,x,y,radius):
+        self.x=x
+        self.y=y
+        self.radius=radius
+        self.vx=200
+        self.vy=0
+    #function for drawing
+    def draw(self):
+        #generating random colors
+        r=random.randint(0,255)
+        g=random.randint(0,255)
+        b=random.randint(0,255)
+        comc=r,g,b
+        pos=(self.x,self.y)
+        screen.draw.filled_circle(pos,self.radius,comc)
+#object creation
+circ1=bouncy(60,60,50)
+circles=[circ1]
+def draw():
+    circ1.draw()
+#defining update
+def update(ti):
+    global circles
+    for i in circles:
+        uy = i.vy
+        i.vy += grav * ti
+        i.y += (uy + i.vy) * 0.5 * ti
+    #handeling bouncing
+        if i.y>800:
+            i.y=800
+            i.vy=-i.vy*0,9
+        i.x+=i.vx*ti
+    
+    
+    
+pgzrun.go()
